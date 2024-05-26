385fa10d22fea0110a962bcd6f7d118fe5263c05
olgakonieva <konievaolga@gmail.com>
2024-05-26 23:14:39 +0300

    I finished my home work

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int height =12;
+        int width =13;
+        int price =40;
+        int tax = 5;
+
+        int image = height + width;
+        int amount = price + tax;
+        image +=1;
+        amount -=2;
+        System.out.println(amount>image);
+        int test =2;
+        if (image%test == 0 || amount%test == 0) {System.out.println("true");}
+        else if (image%test != 0 && amount%test != 0){System.out.println("false");}
+
+        }
+    }
